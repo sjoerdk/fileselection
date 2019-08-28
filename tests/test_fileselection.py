5ac7a3b44047dbc14cb7071d0a5254a7c3ed1ce8
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2019-08-28 14:59:17 +0200

    Renames main module

--- tests/test_fileselection.py
+++ tests/test_fileselection.py
@@ -5,7 +5,7 @@
 from pathlib import Path
 
 import pytest
-from fileselection.fileselectionfolder import (
+from fileselection.fileselection import (
     FileSelectionFile,
     NotRelativeToRootException,
     FileSelectionFolder,
